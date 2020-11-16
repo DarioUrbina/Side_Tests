c7f9824aad7a7ccf57b262e805bd9b7828b33645
Darío Urbina <urbiname@usc.edu>
2020-11-15 18:03:54 -0800

    Adding Reverse_integer

---
+++ Python tests/Reverse_integer.py
@@ -0,0 +1,44 @@
+#Given a 32-bit signed integer, reverse digits of an integer.
+
+#Assume we are dealing with an environment that could only store integers within the 32-bit
+#signed integer range: [−2**31,  2**31 − 1]. For the purpose of this problem,
+#assume that your function returns 0 when the reversed integer overflows.
+
+a = 123456111
+
+#int_str =str(a)
+
+digits = [int(d) for d in str(a)]
+
+print(len(digits),"\n")
+length = len(digits)
+
+for i in digits:
+    print(digits[len(digits)-i])
+    digits_2 = digits[len(digits)-i]
+
+#print(digits_2)
+#for i in digits:
+        
+
+#lenght = len(int_str)
+
+#for i in 
+
+#print(int_str.split(,lenght))
+
+
+#if int < 2**32:
+    
+
+
+
+    #for j in nums:              #2
+     #   if index_2 != index_1:              #3 (if)
+        
+    
+
+
+
+
+    

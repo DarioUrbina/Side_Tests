939f14e76ed68fdeced5e8af92c41b9405f4edc9
Darío Urbina <urbiname@usc.edu>
2020-11-15 18:03:19 -0800

    Two Sum.py

--- Python tests/Two Sum.py
+++ Python tests/Two Sum.py
@@ -1,20 +1,20 @@
-nums = [1,2,4,3]
-target = 7
+nums = [3,3]
+target = 6
 index_1 = 0
 index_2 = 0
 stop = 0
 
 for i in nums:                  #1
     for j in nums:              #2
-        index_2=index_2+1
-        if j != i:              #3 (if)
+        if index_2 != index_1:              #3 (if)
             shoot = i+j
             if shoot == target: #4 (if)
                 stop = 1
                 break
+        index_2=index_2+1
     if stop == 1:
         print(index_1)
-        print(index_2-1)
+        print(index_2)
         break
     index_1=index_1+1
     index_2=0

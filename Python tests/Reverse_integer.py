b8c1a68132abd36de9078e51c33af520b2f7463d
Darío Urbina <urbiname@usc.edu>
2020-11-17 10:34:25 -0800

    integer inversed into another integer

--- Python tests/Reverse_integer.py
+++ Python tests/Reverse_integer.py
@@ -5,56 +5,21 @@
 #assume that your function returns 0 when the reversed integer overflows.
 
 a = 423456112
-digits_b=[]
-count = 1
+b = 0
+count = 0
 
-print("\na: ",a," a  type: ",type(a))
-#int_str =str(a)
+print("\na: ",a,", type of a: ",type(a))
 
 digits = [int(d) for d in str(a)]  #creating a list of of the digits of a
-print("\ndigits lenght: ",len(digits), "digits  type: ",type(digits),"\n")
-
-length = len(digits)
+print("\ndigits lenght: ",len(digits), ", digits  type: ",type(digits),"\n")
 
 for i in digits:
-    #print("count: ",count)
-    print(digits[len(digits)-count])
-    digits_b.append(digits[len(digits)-count])
-    digits_2 = digits[len(digits)-i] 
-    
+
     factor = 10**count
-    #b = +prev + digits[len(digits)-count]* (10**count)
+    b = b + ( digits[count] * (10**count) )
     count=count+1
-    #digits_b =
-
-print("\n",digits[0], "type: ", type(digits[0]))
-print("\nb: ",digits_b)
+    #print("count: " ,count)
 
 
+print("b: ",b,", type of b: ",type(b))
 
-#print(digits_2)
-#for i in digits:
-        
-
-#lenght = len(int_str)
-
-#for i in 
-
-#print(int_str.split(,lenght))
-
-
-#if int < 2**32:
-
-    
-
-
-
-    #for j in nums:              #2
-     #   if index_2 != index_1:              #3 (if)
-        
-    
-
-
-
-
-    

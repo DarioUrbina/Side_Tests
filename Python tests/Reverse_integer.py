0a69f7937c0e2695201a827534270bb6f05953a2
Darío Urbina <urbiname@usc.edu>
2020-11-17 10:18:04 -0800

    basis for creating second reversed integer

--- Python tests/Reverse_integer.py
+++ Python tests/Reverse_integer.py
@@ -4,21 +4,33 @@
 #signed integer range: [−2**31,  2**31 − 1]. For the purpose of this problem,
 #assume that your function returns 0 when the reversed integer overflows.
 
-a = 123456111
+a = 423456112
+digits_b=[]
 count = 1
+
 print("\na: ",a," a  type: ",type(a))
 #int_str =str(a)
 
-digits = [int(d) for d in str(a)] 
+digits = [int(d) for d in str(a)]  #creating a list of of the digits of a
 print("\ndigits lenght: ",len(digits), "digits  type: ",type(digits),"\n")
 
 length = len(digits)
 
 for i in digits:
-    print("count: ",count)
+    #print("count: ",count)
     print(digits[len(digits)-count])
-    digits_2 = digits[len(digits)-i]
+    digits_b.append(digits[len(digits)-count])
+    digits_2 = digits[len(digits)-i] 
+    
+    factor = 10**count
+    #b = +prev + digits[len(digits)-count]* (10**count)
     count=count+1
+    #digits_b =
+
+print("\n",digits[0], "type: ", type(digits[0]))
+print("\nb: ",digits_b)
+
+
 
 #print(digits_2)
 #for i in digits:

a33b364fea915dde8bb666d4f033fafd10e72051
Darío Urbina <urbiname@usc.edu>
2020-12-09 15:57:33 -0800

    heatmap created

--- R/heatmaps, confusion matricex.R
+++ R/heatmaps, confusion matricex.R
@@ -8,3 +8,17 @@ for (i in 1:nrow(tb)) {
     count <- count+1
   }
 }
+
+colors = heat.colors(256)
+
+str(mtcarr)
+rownames(mtcars)    
+colnames(mtcars)
+as.matrix(mtcars)
+heatmap(as.matrix(mtcars)
+     ,scale = "column"
+     ,col = heat.colors(256)
+     ,main = "Characteristics of car models"
+     ,Rowv = NA
+     ,Colv = NA
+     )

89d389064c7f0294460c75e3d17b963783b0ba8e
Darío Urbina <urbiname@usc.edu>
2020-12-11 12:22:49 -0800

    R tests

--- R/heatmaps, confusion matricex.R
+++ R/heatmaps, confusion matricex.R
@@ -11,13 +11,28 @@ for (i in 1:nrow(tb)) {
 
 colors = heat.colors(256)
 
-str(mtcarr)
+str(mtcars)
+
 rownames(mtcars)    
 colnames(mtcars)
 as.matrix(mtcars)
+
+normalize <- function(x){
+  return ((x-min(x))/(max(x)-min(x)))
+}
+
+for (i in 1:nrow(tb)){
+  tb[,c(i)]<-normalize(tb[,c(i)])
+}
+
+#normalize(tb[,c("1")])
+
+zmp.class <- simple_zmp_locations_exp_2.data[,c("ZMP_location")]
+simple_zmp_locations_exp_2.data$LC_1
+
 heatmap(as.matrix(mtcars)
      ,scale = "column"
-     ,col = heat.colors(256)
+     ,col = rev(heat.colors(256))
      ,main = "Characteristics of car models"
      ,Rowv = NA
      ,Colv = NA

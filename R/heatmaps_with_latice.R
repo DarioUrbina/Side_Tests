4a867ac46c9ab0af91e26d993054b160c4cae3d9
Darío Urbina <urbiname@usc.edu>
2020-12-13 16:36:33 -0800

    Heatmaps with lattice

---
+++ R/heatmaps_with_latice.R
@@ -0,0 +1,22 @@
+library("lattice")
+x <- seq(1,10, length.out=20)
+y <- seq(1,10, length.out=20)
+data <- expand.grid(X=x, Y=y)
+data$Z <- runif(400, 0, 5)
+levelplot(Z ~ X*Y, data=data  ,xlab="X",
+          main="")
+
+data <- matrix(runif(100, 0, 5) , 10 , 10)
+colnames(data) <- letters[c(1:10)]
+rownames(data) <- paste( rep("row",10) , c(1:10) , sep=" ")
+
+# plot it flipping the axis
+levelplot(data)
+
+data <- matrix(runif(100, 0, 5) , 10 , 10)
+colnames(data) <- letters[c(1:10)]
+rownames(data) <- paste( rep("row",10) , c(1:10) , sep=" ")
+
+# plot it flipping the axis
+levelplot( t(data[c(nrow(data):1) , ]),
+           col.regions=heat.colors(100))

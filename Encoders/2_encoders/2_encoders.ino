561cc0cfbebaa0b1986b5065c333f6f4a8f29029
Darío Urbina <urbiname@usc.edu>
2021-05-18 13:29:21 -0700

    checkpoint initial commit of the day

--- Encoders/2_encoders/2_encoders.ino
+++ Encoders/2_encoders/2_encoders.ino
@@ -11,6 +11,13 @@ const float convRatio = 0.008371;
 #define readA bitRead(PIND,2)//faster than digitalRead()
 #define readB bitRead(PIND,3)//faster than digitalRead()
 
+//motor
+int PWM = 50;
+int enA=5;  // PWM pin
+int in1=7;
+int in2=8;
+float pwmValA=(PWM*255)/100;
+
 
 void setup() {
   Serial.begin (115200);
@@ -20,6 +27,14 @@ void setup() {
  
   attachInterrupt(digitalPinToInterrupt(encoderPinA), isrA, HIGH);
   attachInterrupt(digitalPinToInterrupt(encoderPinB), isrB, HIGH);
+  //motor
+  pinMode(enA,OUTPUT);
+  pinMode(in1,OUTPUT);
+  pinMode(in2,OUTPUT);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+
+  
 }
 
 void loop() {
@@ -33,7 +48,27 @@ void loop() {
     Serial.print(" / ");
     Serial.println(protectedCount);
   }
-  previousCount = protectedCount;
+  
+            if (protectedCount > 1){
+              while (protectedCount > 0){
+              noInterrupts();
+              protectedCount = count;
+              interrupts();
+              if(protectedCount != previousCount) {
+              Serial.print(protectedCount * convRatio);
+              Serial.print(" / ");
+              Serial.println(protectedCount);
+              }
+              analogWrite (enA, pwmValA);
+              digitalWrite(in1,HIGH);
+              digitalWrite(in2,LOW);
+              }
+            } else {
+              analogWrite (enA, 0);
+            }
+
+   previousCount = protectedCount;
+
 }
 
 void isrA() {
